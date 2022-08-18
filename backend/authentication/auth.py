5340b383eaf63c709f4f3cf65145b5b9f4fdf8f5
amisha26 <amisha8299@gmail.com>
2022-08-18 14:06:13 +0530

    added route and try except block in auth

--- backend/authentication/auth.py
+++ backend/authentication/auth.py
@@ -2,15 +2,19 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+@app.route('/login', methods = ['POST'])
 def login():
-    if user in user_db:
-        if user_db[user] == password:
-            return "Success", 200
+    try:
+        if user in user_db:
+            if user_db[user] == password:
+                return "Success", 200
+            else:
+                return jsonify({"msg":"invalid password"}), 400
         else:
-            return jsonify({"msg":"invalid password"})
-    else:
-        return jsonify({"msg":"User does not exist. Signup instead"})
-
+            return jsonify({"msg":"User does not exist. Signup instead"}), 400
+    except Exception as e:
+        print(e)
+        return jsonify({"msg":"Something went wrong"}), 500
 
 
 if __name__=='__main__':
